e8ed132b89364e56f058f1c54510c962e619ce17
sorz <orz@sorz.org>
2015-09-11 18:01:54 +0000

    Display measuring count

--- app/src/main/java/mo/edu/ipm/stud/environmentalsensing/fragments/RecordStatusFragment.java
+++ app/src/main/java/mo/edu/ipm/stud/environmentalsensing/fragments/RecordStatusFragment.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,7 +25,7 @@ import mo.edu.ipm.stud.environmentalsensing.RecordService;
 /**
  * A {@link Fragment} used to display status and stop the running recording task.
  */
-public class RecordStatusFragment extends Fragment {
+public class RecordStatusFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
     static private final String TAG = "RecordStatusFragment";
 
     private OnRecordingStoppedListener callback;
@@ -36,6 +37,7 @@ public class RecordStatusFragment extends Fragment {
             service = ((RecordService.LocalBinder) binder).getService();
             Log.d(TAG, "Service connected.");
             refreshTaskInfo();
+            refreshTaskStatus();
         }
 
         @Override
@@ -44,9 +46,12 @@ public class RecordStatusFragment extends Fragment {
             Log.d(TAG, "Service disconnected.");
         }
     };
+    private SwipeRefreshLayout swipeLayout;
     private TextView textStartTime;
     private TextView textStopTime;
     private TextView textMeasureInterval;
+    private TextView textSuccessCount;
+    private TextView textFailCount;
 
     public interface OnRecordingStoppedListener {
         public void onRecordingStopped();
@@ -87,12 +92,15 @@ public class RecordStatusFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_record_status, container, false);
-
+        swipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_container);
         textStartTime = (TextView) view.findViewById(R.id.text_start_time);
         textStopTime = (TextView) view.findViewById(R.id.text_stop_time);
         textMeasureInterval = (TextView) view.findViewById(R.id.text_measure_interval);
+        textSuccessCount = (TextView) view.findViewById(R.id.text_measure_success_count);
+        textFailCount = (TextView) view.findViewById(R.id.text_measure_fail_count);
         Button buttonStop = (Button) view.findViewById(R.id.button_stop);
 
+        swipeLayout.setOnRefreshListener(this);
         buttonStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -117,6 +125,27 @@ public class RecordStatusFragment extends Fragment {
         textStopTime.setText(dateFormat.format(service.getStopTime()));
         textMeasureInterval.setText(getString(R.string.certain_seconds,
                 (int) service.getInterval() / 1000));
+    }
+
+    private void refreshTaskStatus() {
+        if (textStopTime == null)
+            return;
+        if (service == null) {
+            textSuccessCount.setText("");
+            textFailCount.setText("");
+        }
+        textSuccessCount.setText("" + service.getMeasureSuccessCount());
+        textFailCount.setText("" + service.getMeasureFailCount());
+    }
+
+    @Override
+    public void onRefresh() {
+        if (!RecordService.isRunning()) {
+            callback.onRecordingStopped();
+            return;
+        }
+        refreshTaskStatus();
+        swipeLayout.setRefreshing(false);
     }
 
     @Override
